1ec4f67c11f8d0ffebb54f46aee046a0137a6825
Zalaya <enol.corzal@gmail.com>
2025-07-17 16:29:59 +0200

    feat: add some utility methods that we will need in the program

---
+++ src/constants.py
@@ -0,0 +1,15 @@
+DEFAULT_OUTPUT_FILENAME = "output.txt"
+TEXT_FILE_EXTENSIONS = [
+    ".txt",
+    ".md",
+    ".py",
+    ".json",
+    ".csv",
+    ".html",
+    ".js",
+    ".ts",
+    ".css",
+    ".xml",
+    ".yml",
+    ".yaml"
+]
