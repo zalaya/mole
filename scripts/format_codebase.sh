1dc3209ce8c01b46686868feb44534160274ad57
Zalaya <enol.corzal@gmail.com>
2025-07-18 23:43:33 +0200

    enhancement: add some more prints for the command line interface in the scripts

--- scripts/format_codebase.sh
+++ scripts/format_codebase.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
 
+echo "Activating the virtual environment..."
 source .venv/Scripts/activate
 
+echo "Installing dependencies from requirements.txt..."
 pip install -r requirements.txt
 
+echo "Running Black on the 'src' and 'tests' directories..."
 black ./src
 black ./tests
 
+echo "Deactivating the virtual environment..."
 deactivate
