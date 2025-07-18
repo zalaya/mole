f9325a5a4cad16e8c6f998addd9a92ed86be7dd5
Zalaya <enol.corzal@gmail.com>
2025-07-18 20:04:07 +0200

    feat: first mvp iteration, now it can collect content from files recursively and has extension support

--- src/constants.py
+++ src/constants.py
@@ -1,15 +1,2 @@
-DEFAULT_OUTPUT_FILENAME = "output.txt"
-TEXT_FILE_EXTENSIONS = [
-    ".txt",
-    ".md",
-    ".py",
-    ".json",
-    ".csv",
-    ".html",
-    ".js",
-    ".ts",
-    ".css",
-    ".xml",
-    ".yml",
-    ".yaml"
-]
+ALLOWED_FILE_EXTENSIONS = [".txt", ".json", ".java", ".py"]
+OUTPUT_FILENAME = "output.txt"
