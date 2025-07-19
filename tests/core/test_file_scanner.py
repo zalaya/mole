b5c9609d46a200edd7fdf78f59f6c1811f5fcc4f
Zalaya <enol.corzal@gmail.com>
2025-07-19 11:58:37 +0200

    fix: solve some problems with the tests, we made some more extensions allowed

--- tests/core/test_file_scanner.py
+++ tests/core/test_file_scanner.py
@@ -17,7 +17,7 @@ def test_given_supported_extension_when_checked_then_returns_true(extension):
     assert result is True
 
 
-@pytest.mark.parametrize("extension", [".exe", ".yaml", ".toml"])
+@pytest.mark.parametrize("extension", [".png", ".jpg", ".exe"])
 def test_given_unsupported_extension_when_checked_then_returns_false(extension):
     # Given
     file_path = f"some/path/file{extension}"
@@ -44,7 +44,7 @@ def test_given_directory_with_supported_files_when_find_file_paths_then_returns_
 
 def test_given_directory_with_unsupported_files_when_find_file_paths_then_ignores_them(tmp_path):
     # Given
-    create_files(tmp_path, ["script.yaml", "note.md"])
+    create_files(tmp_path, ["start.exe", "icon.png"])
 
     # When
     result = find_file_paths(str(tmp_path))
@@ -55,7 +55,7 @@ def test_given_directory_with_unsupported_files_when_find_file_paths_then_ignore
 
 def test_given_mixed_directory_when_find_file_paths_then_returns_only_supported(tmp_path):
     # Given
-    created_files = create_files(tmp_path, ["script.yaml", "note.txt"])
+    created_files = create_files(tmp_path, ["start.exe", "note.txt"])
 
     # When
     result = find_file_paths(str(tmp_path))
