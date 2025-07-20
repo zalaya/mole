8fdd3e58f52950004389fe827caf25beb5d69a67
Zalaya <enol.corzal@gmail.com>
2025-07-20 23:36:43 +0200

    enhancement: remove some tests, we won't need them till we finish

--- scripts/format_codebase.sh
+++ scripts/format_codebase.sh
@@ -6,9 +6,8 @@ source .venv/Scripts/activate
 echo "Installing dependencies from requirements.txt..."
 pip install -r requirements.txt
 
-echo "Running Black on the 'src' and 'tests' directories..."
+echo "Running Black on the 'src' directory..."
 black ./src
-black ./tests
 
 echo "Deactivating the virtual environment..."
 deactivate
